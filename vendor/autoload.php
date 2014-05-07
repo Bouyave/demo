6414883df9c044c03bf06f9a68903f3f2e938f89
Bouyave <benjamin.bfernandes@gmail.fr>
2014-05-07 17:33:04 +0200

    Init

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit8b12dc1f53adbe7c6c3fb41a8327c281::getLoader();
+return ComposerAutoloaderInit2997f98a059a629a97d5237b26804c37::getLoader();
